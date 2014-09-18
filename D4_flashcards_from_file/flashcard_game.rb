09f9a4567c9b02876c9c3a8689ddc012c2a8330d
ChenHuang <chenhuang@ChenHuangs-MacBook-Pro.local>
2014-09-18 13:59:00 -0400

    Deleting everything except the class itself

--- D4_flashcards_from_file/flashcard_game.rb
+++ D4_flashcards_from_file/flashcard_game.rb
@@ -1,6 +1,3 @@
-require "./deck"
-require "./card"
-
 class FlashcardGame
   def initialize(deck)
     @decks = deck
@@ -35,16 +32,3 @@ class FlashcardGame
     @decks.find { |deck| deck.name == request_deck }
   end
 end
-
-decks = []
-spanish_cards = []
-japanese_cards = []
-spanish_cards << Card.new({front: "Gato", back: "Cat"})
-spanish_cards << Card.new({front: "Perro", back: "Dog"})
-japanese_cards << Card.new({front: "Neko", back: "Cat"})
-japanese_cards << Card.new({front: "Inu", back: "Dog"})
-decks << Deck.new({name: "Japanese", cards: japanese_cards})
-decks << Deck.new({name: "Spanish", cards: spanish_cards})
-
-game = FlashcardGame.new(decks)
-game.play
