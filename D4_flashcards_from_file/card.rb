1cfc3bc0437b512c94089355e28097b86d6e900c
ChenHuang <chenhuang@ChenHuangs-MacBook-Pro.local>
2014-09-18 14:54:22 -0400

    Adding user guessing, and comparing with result to see if it's right

--- D4_flashcards_from_file/card.rb
+++ D4_flashcards_from_file/card.rb
@@ -5,6 +5,24 @@ class Card
   end
 
   def play
-    puts "Front is #{@front}, Back is #{@back}"
+    result = ask_user_for_answer
+    if_result_is_right(result)
+  end
+
+  def ask_user_for_answer
+    print "#{@front}: "
+    gets.chomp
+  end
+
+  def if_result_is_right(result)
+    loop do
+      if result == @back
+        puts "You are right!"
+        break
+      else
+        puts "You are wrong, please try again"
+        result = gets.chomp
+      end
+    end
   end
 end
