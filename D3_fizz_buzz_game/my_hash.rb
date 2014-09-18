48beabd949c09dfec23e3719a48b590d49c90fd2
ChenHuang <chenhuang@ChenHuangs-MacBook-Pro.local>
2014-09-18 08:37:32 -0400

    Adding private method found_pair_with_key following DRY

--- D3_fizz_buzz_game/my_hash.rb
+++ D3_fizz_buzz_game/my_hash.rb
@@ -6,13 +6,25 @@ class MyHash
   end
 
   def [](key)
-    found_pair = @pairs.find do |pair|
+    found_pair = found_pair_with_key(key)
+    found_pair.value
+  end
+
+  def []=(key, new_value)
+    found_pair = found_pair_with_key(key)
+    found_pair.value = new_value
+  end
+
+  private
+
+  def found_pair_with_key(key)
+    @pairs.find do |pair|
       pair.key == key
     end
-    found_pair.value
   end
 end
 
 myhash = MyHash.new
 puts "Chen's age is #{myhash["Chen"]}"
-
+myhash["Chen"] = 25.5
+puts "Chen's age is #{myhash["Chen"]}"
